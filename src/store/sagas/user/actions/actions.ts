795a5e90dd437bc63b350294c0b0b27d8dbf0d73
Danil Nepomnyashih <nepomnachihd@gmail.com>
2021-05-27 12:33:47 +0600

    Created actions using redux-toolkit

--- src/store/sagas/user/actions/actions.ts
+++ src/store/sagas/user/actions/actions.ts
@@ -1,5 +1,5 @@
-export const USER_UPLOAD_AVATAR = 'user/upload-avatar';
+import { createAction } from "@reduxjs/toolkit";
 
-export const userUploadAvatar = (payload: File) => {
-  return {type: USER_UPLOAD_AVATAR, payload};
-};
+export const USER_UPLOAD_AVATAR = "user/upload-avatar";
+
+export const userUploadAvatar = createAction<File>(USER_UPLOAD_AVATAR);
